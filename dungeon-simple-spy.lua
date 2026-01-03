b2b4eca4da56b0019d3c988eb23650dd439b05b2
dripnodrizzle <101456710+dripnodrizzle@users.noreply.github.com>
2026-01-03 22:12:56 +0000

    Add ultra-simple dungeon spy for low-level executors
    - Minimal syntax, basic Lua only
    - Directly monitors game.ReplicatedStorage.Events.Dungeon
    - No complex string operations or method chains
    - Simple print-based logging

---
+++ dungeon-simple-spy.lua
@@ -0,0 +1,67 @@
+-- Simple Dungeon Spy
+print("Dungeon Spy Starting...")
+
+local events = {}
+
+-- Find Events folder
+local RS = game.ReplicatedStorage
+local eventsFolder = RS.Events
+
+if not eventsFolder then
+    print("Events folder not found!")
+    return
+end
+
+print("Found Events folder")
+
+-- Find Dungeon events
+local dungeonFolder = eventsFolder.Dungeon
+if not dungeonFolder then
+    print("No Dungeon folder found")
+    return
+end
+
+print("Monitoring Dungeon events...")
+
+-- Hook each remote in Dungeon folder
+for i, remote in pairs(dungeonFolder:GetChildren()) do
+    if remote.ClassName == "RemoteEvent" then
+        local remoteName = remote.Name
+        local oldFire = remote.FireServer
+        
+        remote.FireServer = function(self, ...)
+            local args = {...}
+            local logEntry = {
+                name = remoteName,
+                args = args,
+                time = tick()
+            }
+            table.insert(events, logEntry)
+            
+            print("[DUNGEON EVENT] " .. remoteName)
+            for j, arg in pairs(args) do
+                print("  Arg " .. tostring(j) .. ": " .. tostring(arg))
+            end
+            
+            return oldFire(self, ...)
+        end
+        
+        print("Hooked: " .. remoteName)
+    end
+end
+
+-- Show events function
+_G.ShowEvents = function()
+    print("\n=== CAPTURED EVENTS ===")
+    print("Total: " .. tostring(#events))
+    for i, event in pairs(events) do
+        print("\n[" .. tostring(i) .. "] " .. event.name)
+        for j, arg in pairs(event.args) do
+            print("  " .. tostring(j) .. " = " .. tostring(arg))
+        end
+    end
+    print("======================\n")
+end
+
+print("\nDungeon Spy Active!")
+print("Use: _G.ShowEvents()")
